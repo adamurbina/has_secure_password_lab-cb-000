8427b0ebf2e2757fdf4514cc044e152f550e7c66
Adam Urbina <adamurbina@gmail.com>
2018-11-24 21:24:35 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  post 'create' => 'users#create'
+  resources :users, :sessions
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
