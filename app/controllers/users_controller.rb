c2adad57a685f3c6b04cc59acb0f88a23785009f
Adam Urbina <adamurbina@gmail.com>
2018-11-24 21:58:03 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,7 @@ class UsersController < ApplicationController
   def create
       @user = User.create(user_params)
       if @user.save
-        binding.pry
-        redirect_to action: 'create', controller: 'sessions', name: @user.name, password: @user.password
+        redirect_to 'sessions/create'
       else
         redirect_to new_user_url
       end
